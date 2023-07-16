0213591c2d91b507caefe2a36c7257e08f18c185
Pranjali Maske <pranjalimaske03@gmail.com>
2023-07-16 14:35:00 +0530

    encapsulation

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,16 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        // Task 3
+        RWOnly obj = new RWOnly();
+
+        // Task 4
+        // obj.name = "John"; // Error: The variable name is private and cannot be accessed directly
+        // System.out.println(obj.name); // Error: The variable name is private and cannot be accessed directly
+
+        // Task 6
+        obj.setName("John");
+        System.out.println(obj.getName()); // Output: John
+    }
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,14 @@
 package com.driver;
 
 public class RWOnly {
-    
+     String name; // Task 2
+
+    // Task 5: Getter and Setter
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
